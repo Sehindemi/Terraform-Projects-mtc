114e21d55f8f650d8f55638b36dc9d59070c8ed8
Ubuntu <ubuntu@ip-172-31-52-15.ec2.internal>
2024-02-28 09:10:00 +0000

    NON dry code added the use of the join function to add the ipp address and ports togther, also added an extra container 2

--- main.tf
+++ main.tf
@@ -12,22 +12,53 @@ provider "docker" {}
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "random_string" "random2" {
+  length  = 6
+  special = false
+  upper   = false
+}
 
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.image_id
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    # external = 1880
   }
 }
 
 output "IP-Address" {
-  value = join(":", [docker_container.nodered_container.network_data[0].ip_address, docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container.network_data[0].ip_address, docker_container.nodered_container.ports[0].external])
   description = "The IP address and external ports of the container"
 }
 
 output "container-name" {
-  value = docker_container.nodered_container.name
+  value       = docker_container.nodered_container.name
+  description = "The name of the container"
+}
+
+output "ip_address2" {
+  value       = join(":", [docker_container.nodered_container2.network_data[0].ip_address, docker_container.nodered_container2.ports[0].external])
+  description = "The IP address and external ports of the container"
+}
+
+
+output "container-name2" {
+  value       = docker_container.nodered_container2.name
   description = "The name of the container"
 }
