bb098d40f1be91f08e4bf811755f5df84a9be3bf
Sehindemi <Sehindemitech@gmail.com>
2024-03-06 11:59:20 +0000

    made use of the path.cwd module

--- main.tf
+++ main.tf
@@ -19,22 +19,22 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 resource "random_string" "random" {
-  count   = var.container_count
+  count   = local.container_count
   length  = 4
   special = false
   upper   = false
 }
 
 resource "docker_container" "nodered_container" {
-  count = var.container_count
+  count = local.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.image_id
   ports  {
     internal = var.int_port
-    external = var.ext_port
+    external = var.ext_port[count.index]
   }
   volumes {
     container_path = "/data"
-    host_path = "/home/ubuntu/environment/terraform-docker/noderedvol"
+    host_path = "${path.cwd}/noderedvol"
   }
 }
