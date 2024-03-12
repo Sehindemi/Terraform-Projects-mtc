fe22030950b3b2ba1a1f46c55ef87f7ef8bff9e8
Sehindemi <Sehindemitech@gmail.com>
2024-03-12 08:57:09 +0000

    removed the provider block removed the image block onto the image module folder instaed

--- main.tf
+++ main.tf
@@ -1,23 +1,13 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
-    }
-  }
-}
-
-provider "docker" {}
-
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
     command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol"
   }
 }
 
-resource "docker_image" "nodered_image" {
-  name = var.image[terraform.workspace]
+module "image"{
+  source = "./image"
 }
+
 resource "random_string" "random" {
   count   = local.container_count
   length  = 4
@@ -28,7 +18,7 @@ resource "random_string" "random" {
 resource "docker_container" "nodered_container" {
   count = local.container_count
   name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image = docker_image.nodered_image.image_id
+  image = module.image.image_out
   ports  {
     internal = var.int_port
     external = var.ext_port[terraform.workspace][count.index]
