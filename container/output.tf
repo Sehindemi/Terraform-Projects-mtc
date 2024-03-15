f269486d69211bbf4d0fc549787fb830461a7ca6
Sehindemi <Sehindemitech@gmail.com>
2024-03-15 08:00:58 +0000

    The root code was moved to the modules, which includes the main terraform code for the contianers, the variables which was dependent on the containers and the output values

---
+++ container/output.tf
@@ -0,0 +1,10 @@
+output "ip_address" {
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.network_data[0]["ip_address"], i.ports[0]["external"]])] 
+  description = "The IP address and external ports of the container"
+  
+}
+
+output "container-name" {
+  value       = docker_container.nodered_container.name
+  description = "The name of the container"
+}
