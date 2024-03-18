38cad4d6088350454e015c93123238744e8c0246
Sehindemi <Sehindemitech@gmail.com>
2024-03-18 08:42:16 +0000

    Made use of the for each on the images to refrence both the nodered and influxdb images

--- variables.tf
+++ variables.tf
@@ -1,32 +1,38 @@
 variable "image" {
-  type = map(string)
-  description = "Image for conatiner"
+  type        = map
+  description = "Image for container"
   default = {
-    dev = "nodered/node-red:latest"
-    prod = "nodered/node-red:latest-minimal"
+    nodered = {
+      dev  = "nodered/node-red:latest"
+      prod = "nodered/node-red:latest-minimal"
+    }
+    influxdb = {
+      dev  = "quay.io/influxdb/influxdb:v2.0.2"
+      prod = "quay.io/influxdb/influxdb:v2.0.2"
+    }
   }
 }
 
 variable "ext_port" {
-  type    = map
-  
+  type = map(any)
+
   validation {
-    condition = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
-    error_message = "The External port must be between the valid port range of 0 - 65535." 
+    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+    error_message = "The External port must be between the valid port range of 0 - 65535."
   }
-  
-    validation {
-    condition = max(var.ext_port["prod"]...) <= 1980 && min(var.ext_port["prod"]...) >= 1880
-    error_message = "The External port must be between the valid port range of 0 - 65535." 
+
+  validation {
+    condition     = max(var.ext_port["prod"]...) <= 1980 && min(var.ext_port["prod"]...) >= 1880
+    error_message = "The External port must be between the valid port range of 0 - 65535."
   }
 }
 
 variable "int_port" {
   type    = number
   default = 1880
-  
+
   validation {
-    condition = var.int_port == 1880
+    condition     = var.int_port == 1880
     error_message = "The internal port must be 1880"
   }
 }
